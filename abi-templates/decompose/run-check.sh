a8015b924e89c4e1798b3a755e393bbff06c3aa2
Harumichi kyotoh <harumichi.kyotoh@gmail.com>
2025-09-10 00:15:13 +0900

    Fix header path for decompositionMethod.H in CI

--- abi-templates/decompose/run-check.sh
+++ abi-templates/decompose/run-check.sh
@@ -2,7 +2,8 @@
 set -e
 
 echo "Compiling decomposition ABI test code..."
-g++ -I$WM_PROJECT_DIR/src/OpenFOAM/lnInclude \
-    -I$WM_PROJECT_DIR/src/OSspecific/POSIX/lnInclude \
-    -I$WM_PROJECT_DIR/src/decompositionMethods/lnInclude \
+
+g++ -I$GITHUB_WORKSPACE/src/OpenFOAM/lnInclude \
+    -I$GITHUB_WORKSPACE/src/parallel/decompose/decompositionMethods/lnInclude \
+    -I$GITHUB_WORKSPACE/src/OSspecific/POSIX/lnInclude \
     -c check_Decompose.c -o check_Decompose.o
