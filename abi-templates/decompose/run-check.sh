15858358239718b662ac35c66c702b6f8d14d191
Harumichi kyotoh <harumichi.kyotoh@gmail.com>
2025-09-10 00:42:18 +0900

    Add objectRegistry include path for decomposition ABI CI

--- abi-templates/decompose/run-check.sh
+++ abi-templates/decompose/run-check.sh
@@ -6,5 +6,6 @@ echo "Compiling decomposition ABI test code..."
 g++ -I$GITHUB_WORKSPACE/src/OpenFOAM/lnInclude \
     -I$GITHUB_WORKSPACE/src/parallel/decompose/decompositionMethods/lnInclude \
     -I$GITHUB_WORKSPACE/src/OpenFOAM/meshes/polyMesh \
+    -I$GITHUB_WORKSPACE/src/OpenFOAM/db/objectRegistry \
     -I$GITHUB_WORKSPACE/src/OSspecific/POSIX/lnInclude \
     -c check_Decompose.c -o check_Decompose.o
